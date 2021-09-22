cefe13cf355477e67b9ddcc2b442e4937e9b9819
JonathanBacklin <Jonbac020809@student.jenseneducation.se>
2021-09-22 22:13:54 +0200

    Navbar+ Main content påbörjad

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+            $('.navbar .menu').toggleClass("active")
+            $('.navbar i').toggleClass("active")
+        })
+    });
